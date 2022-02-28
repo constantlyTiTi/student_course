1c55b12cdf1decdc54c0df6945d14144ad8904a2
ting <tli101@my.centennialcollege.ca>
2022-02-28 00:24:44 -0500

    backend

--- backend/controllers/course-ctrl.js
+++ backend/controllers/course-ctrl.js
@@ -11,29 +11,104 @@ createCourse = (req, res) => {
 
     const course = Course(body)
 
-    //todo
+    if (!course) {
+        return res.status(400).json({ success: false, error: err })
+    }
+
+    course.save()
+    .then(()=>{
+        return res.status(201).json({
+            success: true,
+            id: movie._id,
+            message: 'Course created!',
+        })
+    })
+    .catch(error => {
+        return res.status(400).json({
+            error,
+            message: 'Course not created!',
+        })
+    })
 }
 
 getCourse = (req, res) => {
+    await Course.findOne({ _id: req.params.id }, (err, course) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
 
-    //todo
+        return res.status(200).json({ success: true, data: course })
+    }).catch(err => console.log(err))
 }
 
 getCourseList = (req, res) => {
-
-    //todo
+    await Course.find({}, (err, courses) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+        if (!courses.length) {
+            return res
+                .status(404)
+                .json({ success: false, error: `Course not found` })
+        }
+        return res.status(200).json({ success: true, data: courses })
+    }).catch(err => console.log(err))
 }
 
 deleteCourse = (req, res) => {
+    await Course.findOneAndDelete({ _id: req.params.id }, (err, course) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
 
+        if (!course) {
+            return res
+                .status(404)
+                .json({ success: false, error: `Course not found` })
+        }
 
-    //todo
+        return res.status(200).json({ success: true, data: course })
+    }).catch(err => console.log(err))
 }
 
 updateCourse = (req, res) => {
+    const body = req.body
 
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'You must provide a body to update',
+        })
+    }
 
-    //todo
+    Course.findOne({ _id: req.params.id }, (err, course) => {
+        if (err) {
+            return res.status(404).json({
+                err,
+                message: 'Course not found!',
+            })
+        }
+        course.course_code = body.course_code
+        course.course_name = body.course_name
+        course.section = body.section
+        course.semester = body.semester
+        
+        course
+            .save()
+            .then(() => {
+                return res.status(200).json({
+                    success: true,
+                    id: course._id,
+                    message: 'Course updated!',
+                })
+            })
+            .catch(error => {
+                return res.status(404).json({
+                    error,
+                    message: 'Course not updated!',
+                })
+            })
+    })
 }
 
 module.exports = {
