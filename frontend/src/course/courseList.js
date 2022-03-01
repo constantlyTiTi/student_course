6867dbf4dabf7f5507db6c670719e77f001a08b3
ting <tli101@my.centennialcollege.ca>
2022-02-28 21:15:20 -0500

    list

--- frontend/src/course/courseList.js
+++ frontend/src/course/courseList.js
@@ -2,15 +2,31 @@ import React, { useEffect, useState } from 'react'
 import { Form, Button, FloatingLabel } from "react-bootstrap"
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from 'react-router-dom';
-import {setCourseInfo} from '../redux/course-redux'
-import Course from '../models/course'
+import {getCourses} from '../redux/course-redux'
+import {Course} from '../models/course'
 
 const CourseList =()=> {
-    const course = useSelector((state) => state.course.value)
+    const courses = useSelector((state) => state.courses.value)
     const dispatch = useDispatch()
     const navigate = useNavigate()
 
-    const
+    useEffect (()=>{
+        dispatch(getCourses) 
+
+    }, [])
+
+    const courseListNode = courses.map(i=>{
+        React.createElement()
+    })
+
+    return(
+
+        <>
+        <div ref={this.courseListRef}>
+
+        </div>
+        </>
+    )
 
 }
 
